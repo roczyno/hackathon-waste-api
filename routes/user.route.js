9ed2fbb21074a61eb6c2c7c58f487144a3cde46f
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-11-07 22:43:26 +0000

    added new faetures

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,15 +6,17 @@ import {
   getUsers,
   updateUser,
 } from "../controllers/user.controller.js";
-import { verifyAccessTokenAndAdmin, verifyAccessTokenAndAuthorization} from "../middlewares/verifyAccessToken.js"
-
+import {
+  verifyAccessTokenAndAdmin,
+  verifyAccessTokenAndAuthorization,
+} from "../middlewares/verifyAccessToken.js";
 
 const router = express.Router();
 
-router.get("/all",verifyAccessTokenAndAdmin, getUsers);
-router.get("/:id",verifyAccessTokenAndAdmin, getUser);
-router.put("/:id",verifyAccessTokenAndAuthorization, updateUser);
-router.delete("/:id",verifyAccessTokenAndAuthorization,deleteUser);
-router.get("/total",countUsers)
+router.get("/all", verifyAccessTokenAndAdmin, getUsers);
+router.get("/:id", verifyAccessTokenAndAdmin, getUser);
+router.put("/:id", verifyAccessTokenAndAuthorization, updateUser);
+router.delete("/:id", verifyAccessTokenAndAuthorization, deleteUser);
+router.get("/", countUsers);
 
 export default router;
