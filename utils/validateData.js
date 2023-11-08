03d85dee17b59e0c873c362d8e62d79eb7098026
roczyno <adiabaroczyno9@gmail.com>
2023-11-08 10:02:03 +0000

    updated the user model

--- utils/validateData.js
+++ utils/validateData.js
@@ -3,11 +3,9 @@ import passswordComplexity from "joi-password-complexity";
 
 export const validateData = (data) => {
   const schema = joi.object({
-    username: joi.string().required().label("username"),
     email: joi.string().required().label("email"),
     password: passswordComplexity().required().label("password"),
-    location: joi.string().required().label("location"),
-    date: joi.string().required().label("date"),
+    confirmPassword: joi.string().required().label("confirmPassword"),
     appType:joi.string().required().label("appType"),
   });
 
