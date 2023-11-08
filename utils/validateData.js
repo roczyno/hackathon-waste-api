e1d690131e7b49399eeb3a3f3a9cfa556a4baab5
roczyno <adiabaroczyno9@gmail.com>
2023-11-08 10:06:43 +0000

    df

--- utils/validateData.js
+++ utils/validateData.js
@@ -3,6 +3,7 @@ import passswordComplexity from "joi-password-complexity";
 
 export const validateData = (data) => {
   const schema = joi.object({
+    username: joi.string().required().label("username"),
     email: joi.string().required().label("email"),
     password: passswordComplexity().required().label("password"),
     confirmPassword: joi.string().required().label("confirmPassword"),
